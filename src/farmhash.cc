d7c557fd7b9c200f9328dc29eac2420a975bfdb6
Andrew Pamment <apamment@yandex.com>
2017-06-21 16:00:35 +1000

    Compile on DragonFly BSD

--- src/farmhash.cc
+++ src/farmhash.cc
@@ -138,7 +138,7 @@
 #define bswap_32(x) BSWAP_32(x)
 #define bswap_64(x) BSWAP_64(x)
 
-#elif defined(__FreeBSD__)
+#elif defined(__FreeBSD__) || defined(__DragonFly__)
 
 #include <sys/endian.h>
 #undef bswap_32
